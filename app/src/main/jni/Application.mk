b4104da0e7504196b1e9ff90dbc6faca13a4ae16
sfalexrog <sfalexrog@gmail.com>
2015-06-21 21:37:02 +0300

    Switch to clang (again, but it seems to work just fine on Windows this time), speed up building a little bit.

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -3,9 +3,9 @@
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 APP_STL := c++_static
 APP_CPPFLAGS += -frtti
-APP_LDFLAGS += -latomic
+APP_LDFLAGS += -latomic -Wl,--threads
 
-NDK_TOOLCHAIN_VERSION := 4.9
+NDK_TOOLCHAIN_VERSION := clang
 #NDK_TOOLCHAIN_VERSION := clang3.4
 #NDK_TOOLCHAIN_VERSION := snapdragonclang3.5
 
