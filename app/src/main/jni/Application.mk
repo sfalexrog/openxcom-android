2ff88e92a8ecddc0f7951d9471b90deff90fe886
sfalexrog <sfalexrog@gmail.com>
2015-07-24 01:00:27 +0300

    Revert to building for android-19.
    android-21 introduces some changes to the system headers, making newer builds incompatible with pre-Android 5.0 systems. In order to maintain compatibility we have to compile with older headers, which will generate a lot more warnings, but will at least work. The resulting binaries seem to be working with Android 5.0+ for now, but that might change in time. So the build system should probably be updated in the future to fix this.

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -15,5 +15,5 @@ APP_ABI := armeabi armeabi-v7a x86 x86_64
 #APP_ABI := armeabi-v7a x86
 #APP_ABI := armeabi-v7a
 
-APP_PLATFORM := android-21
+APP_PLATFORM := android-19
 APP_SHORT_COMMANDS := true
