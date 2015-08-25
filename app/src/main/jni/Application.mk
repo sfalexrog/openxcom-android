5c3890b02095b46beeeff3583ce1f450aa748ab8
sfalexrog <sfalexrog@gmail.com>
2015-08-25 12:11:10 +0300

    Add the ability to set APP_ABI from environment variable, with fallback.
    The relevant environment variable is BUILD_ARCH_LIST

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -11,7 +11,11 @@ NDK_TOOLCHAIN_VERSION := clang
 
 APP_OPTIM := release
 
-APP_ABI := armeabi armeabi-v7a x86 x86_64
+ifeq ($(BUILD_ARCH_LIST),)
+    APP_ABI := armeabi armeabi-v7a x86 x86_64
+else
+    APP_ABI := $(BUILD_ARCH_LIST)
+endif
 #APP_ABI := armeabi-v7a x86
 #APP_ABI := armeabi-v7a
 
